284f63cc4f7b8da9cff3c89275160153777074ec
Urs-Praveen <work.erpraveen@gmail.com>
2022-09-23 23:24:17 +0530

    dom manipulation

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Hack the world!!")
+
+var headerTitle = document.getElementById('main-header')
+headerTitle.style.borderBottom = "solid 3px black"
+
+var item = document.getElementsByClassName('title')
+// console.log(item);
+item[0].style.fontWeight ="bold"
+item[0].style = 'color: #00FF00'
