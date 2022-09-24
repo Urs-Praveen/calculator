b030fcf9c7f4fc43af96dd7c7573680962dd9684
Urs-Praveen <work.erpraveen@gmail.com>
2022-09-24 21:47:32 +0530

    creating div and inserting in html with javascript

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
-// console.log("Hack the world!!")
+//task 7 creating nodes
+var list = document.querySelector('#items')
+//parent node
+//console.log(list.parentNode)
+list.parentNode.style.backgroundColor= '#ccc'
 
-// var headerTitle = document.getElementById('main-header')
-// headerTitle.style.borderBottom = "solid 3px black"
+// console.log(list.parentElement)
+// console.log(list.parentNode)
+//console.log(list.childNodes)
+// console.log(list.children[1])
 
-// var item = document.getElementsByClassName('title')
-// // console.log(item);
-// item[0].style.fontWeight ="bold"
-// item[0].style = 'color: #00FF00'
+// list.children[0].style.backgroundColor ='red'
+// console.log(list.firstChild)
+// console.log(list.firstElementChild)
+// list.firstElementChild.style = 'color:red'
+// console.log(list.lastElementChild)
+// list.lastElementChild.style = 'color:red'
+// //next sibling
+// console.log(list.nextSibling)
+// console.log(list.nextElementSibling)
+// console.log(list.previousSibling)
+// console.log(list.previousElementSibling)
+// list.previousElementSibling.style.color ='green'
+//creating division
+var newDiv = document.createElement('div')
+newDiv.className = 'hello'
+newDiv.id='hello1'
+newDiv.setAttribute('title','Hello')
 
-// var listItem = document.getElementsByClassName('list-group-item')
-// console.log(listItem);
-// var listItem = document.getElementsByClassName('list-group-item')
-// listItem[2].style.backgroundColor ='green'
-// for(let i=0;i<listItem.length ;i++){
-//     if(i == 2){
-//         continue
-//     }
-//     listItem[i].style.fontWeight ='bold'
-//     listItem[i].style ='color : red'
+var textContent = document.createTextNode('hello')
+newDiv.appendChild(textContent)
+console.log(newDiv);
+var container = document.querySelector('ul')
+var pointer= document.querySelector('ul li')
+container.insertBefore(newDiv,pointer)
+var newDiv2 = document.createElement('div')
+newDiv2.className = 'hello'
+newDiv2.id='hello1'
+newDiv2.setAttribute('title','Hello')
 
-// }
-// var li= document.getElementsByTagName('li')
-// //console.log(li)
-// li[4].style.backgroundColor = '#ccc'
+var textContent = document.createTextNode('hello')
+newDiv2.appendChild(textContent)
 
-// var secondChild = document.querySelector('.list-group-item:nth-child(2)')
-// var thirdItem = document.querySelector('.list-group-item:nth-child(3)')
-
-// secondChild.style.color ='green'
-// thirdItem.remove()
-var list = document.querySelectorAll('.list-group-item')
-//console.log(list)
-//list[1].style.color = 'green'
-for(let i=0 ;i<list.length ;i++){
-    if(i%2==0){
-        list[i].style.backgroundColor='green'
-    }
-}
-//querySelector() selects the first element in the html code 
-//querry selector all selects the whole class and convert it into a array that can be iterated 
+var container2 =document.querySelector('header .container')
+var pointer2 =document.querySelector('header h1')
+container2.insertBefore(newDiv2,pointer2)
